59832f5de22a029ddd838afe19c9fd718d7cd967
Robin Gayanger <robin.ga.fr@gmail.com>
2020-02-16 18:54:18 +0100

    Mister Cocktail working delete method at last

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -19,14 +19,13 @@ class DosesController < ApplicationController
 
   def destroy
     @dose = Dose.find(params[:id])
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.destroy
-    redirect_to cocktail_path(@cocktail)
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:description, :ingredient_id, :ingredient_name)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
