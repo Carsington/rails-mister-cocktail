687c9b43109988f8ed9023f22fcc62740b0781a3
Robin Gayanger <robin.ga.fr@gmail.com>
2020-02-17 16:54:28 +0100

    Mister Cocktail slightly improved code, needs front and all

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "cocktails#index"
   resources :cocktails, only: [:new, :create, :index, :show] do
-    resources :doses, only: [:new, :create, :destroy]
+    resources :doses, only: [:new, :create, :destroy], shallow: true
   end
-  resources :doses, only: [:destroy]
+  # resources :doses, only: [:destroy] => needs to be unnested
+  # or "shallow" as specified above
 end
